099fa42edc644a7a1a138b61a44583264b985dc1
Nick J Lyon <nicholasjlyon@gmail.com>
2022-07-07 10:25:52 -0500

    Changed name of `monster_quick` to `monster_stats`

--- R/monster_stats.R
+++ R/monster_stats.R
@@ -1,14 +1,14 @@
-#' @title Monster Quick Build
+#' @title Quickly Identify Monster Statistics
 #'
 #' @description Quickly identify the vital statistics of a single creature worth the provided experience points (XP) or Challenge Rating (CR). Uses the table provided in p. 274-275 of the Dungeon Master's Guide. Accepts Challenge Ratings of 0, '1/8', '1/4, and '1/2' in addition to numbers between 1 and 30. CR is *not necessary* to provide **if** XP is provided.
 #'
-#' @param xp (numeric) Experience Points of monster to quick build
-#' @param cr (numeric) Challenge Rating of monster to quick build. Note that this is NOT necessary if XP is provided
+#' @param xp (numeric) Experience Point value of the monster
+#' @param cr (numeric) Challenge Rating of the monster. Note that this is NOT necessary if XP is provided
 #'
 #' @importFrom magrittr %>%
 #'
 #' @export
-monster_quick <- function(xp = NULL, cr = NULL){
+monster_stats <- function(xp = NULL, cr = NULL){
   # Squelch visible bindings note
   DMG_XP <- NULL
 
