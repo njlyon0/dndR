9bc171a16688e898968d4f04fa2cf0aed507ce4c
Nick J Lyon <nicholasjlyon@gmail.com>
2023-03-24 10:22:57 -0400

    First commit of NPC creator guts (not functional!)

---
+++ R/npc_creator.R
@@ -0,0 +1,47 @@
+
+
+
+# Pick a first name
+
+
+# Pick a last name
+npc_last <- sample(x = c(runebash
+                         steeljumper
+                         dawnflayer
+                         fullbane
+                         willowstone
+                         saurwoods
+                         farsong
+                         grizzlyglory
+                         clawhell
+                         hawkgrain
+                         cindermore
+                         slatesoar
+                         mournbrow
+                         regalvalor
+                         hillthorne
+                         grizzlyspark
+                         longfollower
+                         laughingdane
+                         bluepeak
+                         lightningscream
+                         rockash
+                         windsorrow
+                         nicklebreeze
+                         dewbreath
+                         orbswallow
+                         shieldshot
+                         forestbinder
+                         axeshard
+                         gloomdust
+                         ),
+                   size = 1)
+
+# Pick a race
+npc_race <- sample(x = dndR::dnd_races(), size = 1)
+
+# Pick a career
+npc_role <- sample(x = c("acolyte", "adventurer", "ambassador", "anthropologist", "archaeologist", "artisan", "barber", "bounty hunter", "caravan driver", "caravan guard", "carpenter", "charlatan", "city watch", "criminal", "diplomat", "doctor", "farmer", "farrier", "entertainer", "gladiator", "hermit", "initiate", "inquisitor", "investigator", "knight", "mercenary", "merchant", "navigator", "noble", "outlander", "sage", "scholar", "smith", "soldier", "spy", "student of magic", "smuggler", "urchin", "veteran"),
+                   size = 1)
+
+
