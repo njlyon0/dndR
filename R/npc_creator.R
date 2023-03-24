a68bb065b5c76a795bb6bff2989dc6edf915979e
Nick J Lyon <nicholasjlyon@gmail.com>
2023-03-24 10:37:01 -0400

    Finished `roxygen` format `npc_creator` function

--- R/npc_creator.R
+++ R/npc_creator.R
@@ -1,12 +1,31 @@
+#' @title Create a Non-Player Character (NPC)
+#'
+#' @description Randomly selects a race and job for a user-specified number of NPCs
+#'
+#' @param npc_count (numeric) number of NPCs for which to choose race/positions
+#'
+#' @return (dataframe) dataframe with two columns (one for race and one for job) and a number of rows equal to `npc_count`
+#' @export
+#'
+#' @examples
+#' # Create some information for an NPC
+#' npc_creator(npc_count = 1)
+#'
+npc_creator <- function(npc_count = 1){
 
+  # Error out if count isn't a number
+  if(is.numeric(npc_count) != TRUE)
+    stop("`npc_count` must be numeric")
 
+  # Pick a race
+  npc_race <- sample(x = dndR::dnd_races(), size = npc_count)
 
+  # Pick a career
+  npc_role <- sample(x = c("acolyte", "adventurer", "ambassador", "anthropologist", "archaeologist", "artisan", "barber", "bounty hunter", "caravan driver", "caravan guard", "carpenter", "charlatan", "city watch", "criminal", "diplomat", "doctor", "farmer", "farrier", "entertainer", "gladiator", "hermit", "initiate", "inquisitor", "investigator", "knight", "mercenary", "merchant", "navigator", "noble", "outlander", "sage", "scholar", "smith", "soldier", "spy", "student of magic", "smuggler", "urchin", "veteran"),
+                     size = npc_count)
 
-# Pick a race
-npc_race <- sample(x = dndR::dnd_races(), size = 1)
+  # Assemble into named vector
+  npc_info <- data.frame("Race" = npc_race, "Job" = npc_role)
 
-# Pick a career
-npc_role <- sample(x = c("acolyte", "adventurer", "ambassador", "anthropologist", "archaeologist", "artisan", "barber", "bounty hunter", "caravan driver", "caravan guard", "carpenter", "charlatan", "city watch", "criminal", "diplomat", "doctor", "farmer", "farrier", "entertainer", "gladiator", "hermit", "initiate", "inquisitor", "investigator", "knight", "mercenary", "merchant", "navigator", "noble", "outlander", "sage", "scholar", "smith", "soldier", "spy", "student of magic", "smuggler", "urchin", "veteran"),
-                   size = 1)
-
-
+  # Return the data.frame
+  return(npc_info) }
