5ef0053bcc68eac85142c5dfbde03eb112bb9f46
njlyon0 <njlyon@alumni.iastate.edu>
2023-06-16 12:30:16 -0400

    Created three reasonable plot options

--- dev/probability_plot.R
+++ dev/probability_plot.R
@@ -80,3 +80,34 @@ ggplot(data = result_freq, aes(x = outcome, y = ct, fill = dice_type)) +
 
 
 
+ggplot(roll_results, aes(x = outcome, fill = dice_type, color = dice_type)) +
+  geom_histogram(bins = length(unique(roll_results$outcome)), alpha = 0.5,
+                 boundary = 0.1) +
+  geom_vline(xintercept = med_roll, linetype = 2,
+             color = dice_palette[names(dice_palette) == dice_type]) +
+  scale_fill_manual(values = dice_palette) +
+  scale_color_manual(values = dice_palette) +
+  scale_x_continuous(breaks = unique(roll_results$outcome)) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 15)) +
+  labs(x = "Roll Result", y = paste0("Frequency (", roll_iter, " Rolls)"))
+
+
+
+ggplot(data = result_freq, aes(x = outcome_fact, y = ct, fill = dice_type)) +
+  # Add line for median
+  # geom_vline(xintercept = med_roll, linetype = 2) +
+  # Add points for outcomes
+  geom_bar(stat = 'identity', alpha = 0.8) +
+  # Tweak x-axis
+  # scale_x_discrete(limits = result_freq$outcome) +
+  # Handle plot formatting (consistent with other plotting functions in the package)
+  scale_fill_manual(values = dice_palette) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 15)) +
+  labs(x = "Roll Result", y = paste0("Frequency (", roll_iter, " Rolls)"))
+
