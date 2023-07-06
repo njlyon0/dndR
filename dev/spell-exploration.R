8a8706d04b9758eaf0d6326e689b3f12228ef267
njlyon0 <njlyon@alumni.iastate.edu>
2023-07-06 15:50:03 -0400

    Narrowing in on successful identification of files in a GitHub repo

--- dev/spell-exploration.R
+++ dev/spell-exploration.R
@@ -7,9 +7,47 @@
 
 # Load libraries
 librarian::shelf(tidyverse, dndR)
+# librarian::shelf(oganm/wizaRd)
 
 # Grimoire link (list of 5e spells)
 ## https://github.com/Traneptora/grimoire/tree/master
+
+# Load needed library
+library(httr)
+
+
+library(gh)
+
+grim_conts <- gh::gh("/repos/Traneptora/grimoire/contents")
+str(grim_conts)
+
+
+for(i in 1:length(grim_conts)){
+
+  message("Element ", i, " is ", grim_conts[[i]]$name)
+
+}
+
+
+grim_conts[[11]]
+
+
+# Grab the contents of that repo
+grimoire_gh <- httr::GET(url = paste0("https://api.github.com/repos/Traneptora/grimoire/tree/master", "?recursive=1"))
+grimoire_gh
+
+
+httr::GET(url = "")
+
+# Check for warnings / errors
+httr::stop_for_status(x = grimoire_gh)
+
+# Access files in that link
+httr::content(x = grimoire_gh)$tree
+
+
+
+
 
 # Can read Markdown in GitHub Grimoire from R
 base::readLines(con = url("https://raw.githubusercontent.com/Traneptora/grimoire/master/_posts/2014-08-24-acid-splash.markdown"))
