72ba40bb3920163a84a973a6bbb9096ba3f138d5
njlyon0 <njlyon@alumni.iastate.edu>
2023-12-09 09:04:03 -0500

    Deleting non-function variant

--- R/reroll.R
+++ R/reroll.R
@@ -1,39 +1,26 @@
+#' @title Roll Any Number of Dice
+#'
+#' @description Rolls the specified number and type of dice. Dice are specified in the shorthand common to Dungeons & Dragons (i.e., number of dice, "d", number of faces of those dice). Includes an argument for whether each die's value should be returned as a message (rather than just the total of all dice in the roll). Rolling two twenty-sided dice (i.e., "2d20") is assumed to be rolling with advantage/disadvantage so both numbers are returned.
+#'
+#' @param dice (character) number and type of dice to roll specified in Dungeons & Dragons shorthand (e.g., "2d4" to roll two four-sided dice). Defaults to a single twenty-sided die (i.e., "1d20")
+#' @param show_dice (logical) whether to print the values of each individual die included in the total. Defaults to FALSE
+#'
+#' @return (numeric) sum of specified dice outcomes
+#'
+#' @export
+#'
+#' @examples
+#' # Roll your desired dice
+#' roll(dice = "4d6", show_dice = TRUE)
+
+
+
+
+
+
+
 # Working on a re-roll function
 
-# Start by loading needed libraries
-library(dndR); library(tidyverse)
-
-# Clear environment
-rm(list = ls())
-
-# Make a vector of dice rolls
-(vals <- c(1, 5, 3, 7, 1))
-
-# Define dice type
-dice_type <- "d8"
-
-# Identify number of sides (without "d")
-dice_faces <- as.numeric(gsub(pattern = "d", replacement = "", x = dice_type))
-
-# Scripted variant ----
-
-# If any are 1s...
-if(1 %in% vals){
-
-  # Message
-  message("Re-rolling 1s")
-
-  # Count number of dice to re-roll
-  reroll_ct <- sum(vals == 1, na.rm = F)
-
-  # Re-roll with the specified dice type
-  new_vals <- base::sample(x = 1:dice_faces, size = reroll_ct)
-
-  # Drop 1s in original result
-  no_1s <- setdiff(x = vals, y = 1)
-
-  # Attach the new rolls to the old rolls without 1s
-  full_vals <- c(no_1s, new_vals) }
 
 # Function variant ----
 
