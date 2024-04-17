0f1cd771c0d0713f79e01cf0a2f6d30a47df451c
njlyon0 <njlyon@alumni.iastate.edu>
2024-04-17 10:42:40 -0400

    Tweaked to avoid processing when no monsters match user-provided name (also updated expected column names)

--- dev/monster_text.R
+++ dev/monster_text.R
@@ -34,43 +34,55 @@ monster_text <- function(name = NULL){
   # Perform the desired query
   focal_monster <- all_monsters %>%
     # Subset to only creatures where the name is an exact match
-    dplyr::filter(tolower(monster_name) %in% tolower(name)) %>%
-    # Make all columns characters
-    dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
-                                .fns = as.character)) %>%
-    # Flip it to long format
-    tidyr::pivot_longer(cols = dplyr::everything(.),
-                        names_to = "category", values_to = "info") %>%
-    # Remove all empty rows that creates
-    dplyr::filter(!is.na(info) & nchar(info) > 0) %>%
-    # Make some entries more self-explanatory
-    dplyr::mutate(info = dplyr::case_when(
-      # Combine 'category' with 'info' for some entries
-      category %in% c("STR", "DEX", "CON", "INT", "WIS", "CHA",
-                      "cr", "xp") ~ paste0(toupper(category), ": ", info),
-      # For others, do more customization
-      category %in% c("speed", "size", "type", "skills",
-                      "alignment", "languages"
-                      ) ~ paste0(stringr::str_to_title(string = category), ": ", info),
-      category == "armor_class" ~ paste0("AC: ", info),
-      category == "hit_points" ~ paste0("HP: ", info),
-      category == "damage_immunities" ~ paste0("Damage Immunities:", info),
-      category == "damage_resistances" ~ paste0("Damage Resistances:", info),
-      category == "damage_vulnerabilities" ~ paste0("Damage Vulnerabilities:", info),
-      category == "condition_immunities" ~ paste0("Condition Immunities: ", info),
-      category == "saving_throws" ~ paste0("Saving Throws: ", info),
-      # Otherwise just information is fine
-      T ~ info)) %>%
-    # Ditch category column altogether (now unneeded)
-    dplyr::select(-category) %>%
-    # Make it a dataframe
-    as.data.frame()
+    dplyr::filter(tolower(monster_name) %in% tolower(name))
 
-  # If there is not a spell of that name print a message
+  # If there is not a monster of that name print a message
   if(nrow(focal_monster) == 0){
     message("No monster(s) found matching that name; consider checking spelling")
-    # Otherwise, return the spell's info
-  } else { return(focal_monster) } }
+
+    # Otherwise...
+  } else {
+
+    # Do some tidying to make the output neater
+    tidy_monster <- focal_monster %>%
+      # Make all columns characters
+      dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
+                                  .fns = as.character)) %>%
+      # Flip it to long format
+      tidyr::pivot_longer(cols = dplyr::everything(.),
+                          names_to = "category", values_to = "info") %>%
+      # Remove all empty rows that creates
+      dplyr::filter(!is.na(info) & nchar(info) > 0) %>%
+      # Make some entries more self-explanatory
+      dplyr::mutate(info = dplyr::case_when(
+        # Combine 'category' with 'info' for some entries
+        category %in% c("STR", "DEX", "CON",
+                        "INT", "WIS", "CHA") ~ paste0(category, ": ", info),
+        # For others, do more customization
+        category %in% c("speed", "skills", "languages"
+        ) ~ paste0(stringr::str_to_title(string = category), ": ", info),
+        category == "armor_class" ~ paste0("AC: ", info),
+        category == "hit_points" ~ paste0("HP: ", info),
+        category == "monster_cr" ~ paste0("CR: ", info),
+        category == "monster_xp" ~ paste0("XP: ", info),
+        category == "monster_source" ~ paste0("Source: ", info),
+        category == "monster_size" ~ paste0("Size: ", info),
+        category == "monster_type" ~ paste0("Type: ", info),
+        category == "monster_alignment" ~ paste0("Alignment: ", info),
+        category == "damage_immunities" ~ paste0("Damage Immunities:", info),
+        category == "damage_resistances" ~ paste0("Damage Resistances:", info),
+        category == "damage_vulnerabilities" ~ paste0("Damage Vulnerabilities:", info),
+        category == "condition_immunities" ~ paste0("Condition Immunities: ", info),
+        category == "saving_throws" ~ paste0("Saving Throws: ", info),
+        # Otherwise just information is fine
+        T ~ info)) %>%
+      # Ditch category column altogether (now unneeded)
+      dplyr::select(-category) %>%
+      # Make it a dataframe
+      as.data.frame()
+
+    # Return that information
+    return(tidy_monster) } }
 
 
 # Invoke function
