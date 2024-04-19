f7ef1271417601c70373b478919b6d6a69a17051
njlyon0 <njlyon@alumni.iastate.edu>
2024-04-19 16:00:04 -0400

    Early stages of exploratory work (foundation laying mostly)

--- dev/encounter_creator.R
+++ dev/encounter_creator.R
@@ -26,6 +26,23 @@ rm(list = ls())
 # Scripted Exploration ----
 ## --------------------------- ##
 
+# Define relevant objects
+enc_type <- "undead"
+pc_level <- 5
+pc_size <- 4
+enc_diff <- "medium"
+
+# Subset available creatures to only those of desired 'type'
+creature_sub <- dndR::creatures %>%
+  # Drop all but needed columns
+  dplyr::select(creature_type, creature_name, creature_xp) %>%
+  dplyr::filter(stringr::str_detect(string = creature_type, pattern = enc_type))
+
+# Check available XP manually
+(max_xp <- xp_pool(party_level = pc_level, party_size = pc_size, difficulty = enc_diff) )
+
+# Identify relevant creature(s)
+
 
 
 # End ----
