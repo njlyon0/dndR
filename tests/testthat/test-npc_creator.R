5f3aaba3ee6afc323b63bddd3d44fb6c2667987a
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 16:42:16 -0400

    First round of `npc_creator` tests

---
+++ tests/testthat/test-npc_creator.R
@@ -0,0 +1,23 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-npc_creator.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  expect_error(npc_creator(npc_count = "bbb"))
+})
+
+# # Warning testing
+# test_that("Warnings work as desired", {
+#   # No warnings in this function
+# })
+
+# # Message testing
+# test_that("Messages work as desired", {
+#   # No messages in this function
+# })
+
+# Output testing
+test_that("Outputs are as expected", {
+  expect_equal(nrow(npc_creator(npc_count = 4)), 4)
+  expect_equal(class(npc_creator(npc_count = 5)), "data.frame")
+})
