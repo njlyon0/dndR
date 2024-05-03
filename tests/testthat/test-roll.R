ff9ab992a60044e3b3e20142025a452011e03b63
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 13:47:40 -0400

    Tests for `roll` function

---
+++ tests/testthat/test-roll.R
@@ -0,0 +1,41 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-roll.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  expect_error(roll(dice = 20))
+})
+
+# Warning testing
+test_that("Warnings work as desired", {
+  expect_warning(roll(dice = "1d20", show_dice = "true", re_roll = FALSE))
+  expect_warning(roll(dice = "1d20", show_dice = FALSE, re_roll = "true"))
+})
+
+# Message testing
+test_that("Messages work as desired", {
+  expect_message(roll(dice = "d20"))
+})
+
+# Output testing
+test_that("Outputs are as expected", {
+  ## Rolling 2 d20s should return a dataframe
+  value <- roll(dice = "2d20", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "data.frame")
+
+  ## All other dice types should return a single number
+  value <- roll(dice = "2d4", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+  value <- roll(dice = "3d6", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+  value <- roll(dice = "4d8", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+  value <- roll(dice = "5d10", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+  value <- roll(dice = "6d12", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+  value <- roll(dice = "1d20", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+  value <- roll(dice = "3d20", show_dice = FALSE, re_roll = FALSE)
+  expect_equal(class(value), "integer")
+})
