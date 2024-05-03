3dc08de5a9502af83c1ded098faf51ad42494352
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 13:40:16 -0400

    Improving testing template file

--- dev/test-template.R
+++ dev/test-template.R
@@ -3,15 +3,20 @@
 
 # Error testing
 test_that("Errors work as desired", {
-
+  expect_error()
 })
 
 # Warning testing
 test_that("Warnings work as desired", {
+  expect_warning()
+})
 
+# Message testing
+test_that("Messages work as desired", {
+  expect_message()
 })
 
 # Output testing
 test_that("Outputs are as expected", {
-
+  expect_equal()
 })
