566d04a0ae899057dbf37ba3946d55282eec6a21
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 13:29:56 -0400

    Putting test skeleton in dev folder to make new test development simpler

---
+++ dev/test-template.R
@@ -0,0 +1,17 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-<SCRIPT NAME>.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+
+})
+
+# Warning testing
+test_that("Warnings work as desired", {
+
+})
+
+# Output testing
+test_that("Outputs are as expected", {
+
+})
