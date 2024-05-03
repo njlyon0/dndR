7beaff3db976ea1a865eacf6672e4a3f390f2313
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 16:56:11 -0400

    Unit tests for `monster_creator`

---
+++ tests/testthat/test-monster_creator.R
@@ -0,0 +1,26 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-monster_creator.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  expect_error(monster_creator(party_level = NULL, party_size = NULL))
+  expect_error(monster_creator(party_level = 6, party_size = NULL))
+  expect_error(monster_creator(party_level = NULL, party_size = 4))
+  expect_error(monster_creator(party_level = "six", party_size = 4))
+  expect_error(monster_creator(party_level = 6, party_size = "four"))
+})
+
+# # Warning testing
+# test_that("Warnings work as desired", {
+#   # No warnings in this function
+# })
+
+# # Message testing
+# test_that("Messages work as desired", {
+#   # No messages in this function
+# })
+
+# Output testing
+test_that("Outputs are as expected", {
+  expect_equal(class(monster_creator(party_level = 4, party_size = 4)), "data.frame")
+})
