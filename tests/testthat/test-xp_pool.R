1d4c7af92b30f23a5fe83ed7598070bbb2a3b830
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 13:35:52 -0400

    First pass at `xp_pool` tests

--- tests/testthat/test-xp_pool.R
+++ tests/testthat/test-xp_pool.R
@@ -3,15 +3,31 @@
 
 # Error testing
 test_that("Errors work as desired", {
-
+  ## Any input unspecified
+  expect_error(dndR::xp_pool(party_level = NULL, party_size = 2, difficulty = 'medium'))
+  expect_error(dndR::xp_pool(party_level = 3, party_size = NULL, difficulty = 'medium'))
+  expect_error(dndR::xp_pool(party_level = 3, party_size = 2, difficulty = NULL))
+  ## Party information non-numeric
+  expect_error(dndR::xp_pool(party_level = "x", party_size = 2, difficulty = 'medium'))
+  expect_error(dndR::xp_pool(party_level = 3, party_size = "x", difficulty = 'medium'))
+  ## Too many party levels
+  expect_error(dndR::xp_pool(party_level = 1:10, party_size = 2, difficulty = 'medium'))
+  ## Unsupported difficulty
+  expect_error(dndR::xp_pool(party_level = 3, party_size = 2, difficulty = 'super easy'))
 })
 
-# Warning testing
-test_that("Warnings work as desired", {
-
-})
+# # Warning testing
+# test_that("Warnings work as desired", {
+#   # No warnings in this function
+# })
 
 # Output testing
 test_that("Outputs are as expected", {
+  ## Integer party level
+  available_xp <- dndR::xp_pool(party_level = 5, party_size = 5, difficulty = 'medium')
+  expect_equal(class(available_xp), "numeric")
 
+  ## Non-integer party level
+  available_xp <- dndR::xp_pool(party_level = 3.6, party_size = 5, difficulty = 'medium')
+  expect_equal(class(available_xp), "numeric")
 })
