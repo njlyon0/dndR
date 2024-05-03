5c7c45aa81a215e4c077806a0edcef8ce9d9ad94
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 14:47:48 -0400

    Adding test for `mod_calc`

---
+++ tests/testthat/test-mod_calc.R
@@ -0,0 +1,22 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-mod_calc.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  expect_error(mod_calc(score = "aaa"))
+})
+
+# # Warning testing
+# test_that("Warnings work as desired", {
+#   # No warnings in this function
+# })
+
+# # Message testing
+# test_that("Messages work as desired", {
+#   # No messages in this function
+# })
+
+# Output testing
+test_that("Outputs are as expected", {
+  expect_equal(class(mod_calc(score = 10)), "character")
+})
