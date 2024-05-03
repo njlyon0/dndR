76d540f93d13e1c7ea6867c5a71ed1169d02dbc6
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 16:42:07 -0400

    Handled edge case for non-integer numbers of NPCs and made sure vector sampling is with replacement

--- R/npc_creator.R
+++ R/npc_creator.R
@@ -16,14 +16,18 @@ npc_creator <- function(npc_count = 1){
 
   # Error out if count isn't a number
   if(is.numeric(npc_count) != TRUE)
-    stop("`npc_count` must be numeric")
+    stop("'npc_count' must be numeric")
+
+  # Handle non-integer values
+  if(is.integer(npc_count) != TRUE){
+    npc_count <- ceiling(x = npc_count) }
 
   # Pick a race
-  npc_race <- sample(x = dndR::dnd_races(), size = npc_count)
+  npc_race <- sample(x = dndR::dnd_races(), size = npc_count, replace = TRUE)
 
   # Pick a career
   npc_role <- sample(x = c("acolyte", "adventurer", "ambassador", "anthropologist", "archaeologist", "artisan", "barber", "bounty hunter", "caravan driver", "caravan guard", "carpenter", "charlatan", "city watch", "criminal", "diplomat", "doctor", "farmer", "farrier", "entertainer", "gladiator", "hermit", "initiate", "inquisitor", "investigator", "knight", "mercenary", "merchant", "navigator", "noble", "outlander", "sage", "scholar", "smith", "soldier", "spy", "student of magic", "smuggler", "urchin", "veteran"),
-                     size = npc_count)
+                     size = npc_count, replace = TRUE)
 
   # Assemble into named vector
   npc_info <- data.frame("Race" = npc_race, "Job" = npc_role)
