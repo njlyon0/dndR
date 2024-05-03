e6aaaa7c1ebc17e20872a24f6e5984aead41e02c
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 16:12:00 -0400

    Tests for `race_mod` function

---
+++ tests/testthat/test-race_mod.R
@@ -0,0 +1,26 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-race_mod.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  ## Unspecified PC race
+  expect_error(race_mods(race = NULL))
+  ## Unsupported PC race
+  expect_error(race_mods(race = "fake pc race"))
+})
+
+# # Warning testing
+# test_that("Warnings work as desired", {
+#   # No warnings in function
+# })
+
+# Message testing
+test_that("Messages work as desired", {
+  expect_message(race_mods(race = "random"))
+  expect_message(race_mods(race = "plasmoid"))
+})
+
+# Output testing
+test_that("Outputs are as expected", {
+  expect_equal(class(race_mods(race = "goblin")), "data.frame")
+})
