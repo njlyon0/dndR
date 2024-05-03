680614484a21e0d3f0848550e32da0bc506d0723
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 13:55:44 -0400

    Adding `xp_cost` tests

---
+++ tests/testthat/test-xp_cost.R
@@ -0,0 +1,30 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-xp_cost.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+  ## Any input unspecified
+  expect_error(xp_cost(monster_xp = NULL, monster_count = 3, party_size = 2))
+  expect_error(xp_cost(monster_xp = 100, monster_count = NULL, party_size = 2))
+  expect_error(xp_cost(monster_xp = 100, monster_count = 3, party_size = NULL))
+  ## Any input not numeric
+  expect_error(xp_cost(monster_xp = "xxx", monster_count = 3, party_size = 2))
+  expect_error(xp_cost(monster_xp = 100, monster_count = "xxx", party_size = 2))
+  expect_error(xp_cost(monster_xp = 100, monster_count = 3, party_size = "xxx"))
+})
+
+# Warning testing
+test_that("Warnings work as desired", {
+  expect_warning(xp_cost(monster_xp = 10000, monster_count = 32, party_size = 7))
+})
+
+# # Message testing
+# test_that("Messages work as desired", {
+#   # No messages in this function
+# })
+
+# Output testing
+test_that("Outputs are as expected", {
+  spent_xp <- xp_cost(monster_xp = 650, monster_count = 2, party_size = 5)
+  expect_equal(class(spent_xp), "numeric")
+})
