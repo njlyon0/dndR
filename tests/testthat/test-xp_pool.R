56887e2e340ff366807904e84de138b5c148fa5f
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-03 13:29:35 -0400

    Beginning work on `testthat` architecture for this package (unit testing!)

---
+++ tests/testthat/test-xp_pool.R
@@ -0,0 +1,17 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-xp_pool.R"))
+
+# Error testing
+test_that("Errors work as desired", {
+
+})
+
+# Warning testing
+test_that("Warnings work as desired", {
+
+})
+
+# Output testing
+test_that("Outputs are as expected", {
+
+})
