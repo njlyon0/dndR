c0f5c9de0424f49dc1c5d04314614a9535e4d639
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-06 11:16:57 -0400

    Adding tests to `encounter_creator` that address new behavior with `try` argument

--- tests/testthat/test-encounter_creator.R
+++ tests/testthat/test-encounter_creator.R
@@ -6,18 +6,21 @@
 #   All inputs received from/tested in `xp_pool` and/or `xp_cost`
 # })
 
-# # Warning testing
-# test_that("Warnings work as desired", {
-#   # No warnings in this function
-# })
+# Warning testing
+test_that("Warnings work as desired", {
+  expect_warning(encounter_creator(party_level = 10, party_size = 3,
+                                   difficulty = "deadly", try = "xxx"))
+})
 
-# # Message testing
-# test_that("Messages work as desired", {
-#   # No messages in this function
-# })
+# Message testing
+test_that("Messages work as desired", {
+  expect_message(encounter_creator(party_level = 5, party_size = 5,
+                                   difficulty = "deadly", try = NULL))
+})
 
 # Output testing
 test_that("Outputs are as expected", {
-  my_battle <- encounter_creator(party_level = 10, party_size = 4, difficulty = "hard")
+  my_battle <- encounter_creator(party_level = 10, party_size = 4,
+                                 difficulty = "hard", try = 2)
   expect_equal(class(my_battle), c("tbl_df", "tbl", "data.frame"))
 })
