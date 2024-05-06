c3e27673d7c185f28c323acfa9038976e63e013c
njlyon0 <njlyon@alumni.iastate.edu>
2024-05-06 11:16:24 -0400

    Adding a `try` argument to `encounter_creator` to control efforts to minimize variation between uses of the function (when the same values are passed to the arguments each time)

--- R/encounter_creator.R
+++ R/encounter_creator.R
@@ -5,6 +5,7 @@
 #' @param party_level (numeric) integer indicating the average party level. If all players are the same level, that level is the average party level
 #' @param party_size (numeric) integer indicating how many player characters (PCs) are in the party
 #' @param difficulty (character) one of "easy", "medium", "hard", or "deadly" for the desired difficulty of the encounter
+#' @param try (numeric) integer indicating the number of attempts to make to maximize encounter XP while remaining beneath the threshold defined by the other parameters
 #'
 #' @return (dataframe) creature experience point (XP) values as well as the maximum XP for an encounter of the specified difficulty and the realized XP cost of the returned creatures
 #' @importFrom magrittr %>%
@@ -16,18 +17,21 @@
 #' # Create a hard encounter for a 2-person, 9th level party
 #' encounter_creator(party_level = 9, party_size = 2, difficulty = "hard")
 #'
-encounter_creator <- function(party_level = NULL, party_size = NULL, difficulty = NULL){
-  # Silence visible bindings note
-  creature_xp <- NULL
+encounter_creator <- function(party_level = NULL, party_size = NULL,
+                              difficulty = NULL, try = 5){
 
-  # Identify all possible XP values for creatures
-  available <- data.frame("creature_xp" = c(10, 25, 50, 100, 200, 450,
-                                            700, 1100, 1800, 2300, 2900,
-                                            3900, 5000, 5900, 7200, 8400,
-                                            8900, 10000, 11500, 13000,
-                                            15000, 18000, 20000, 22000, 25000,
-                                            33000, 41000, 50000, 62000, 62500,
-                                            75000, 90000, 105000, 155000, 330000))
+  # Silence visible bindings note
+  creature_xp <- attempt_num <- NULL
+
+  # If 'try' is null, re-set to default
+  if(is.null(try) == TRUE){
+    message("'try' set to 5")
+    try <- 5 }
+
+  # If try is not a number, warn and coerce to 5
+  if(is.numeric(try) != TRUE){
+    warning("'try' must be numeric. Setting to 5")
+    try <- 5 }
 
   # Calculate maximum allowed XP for this encounter
   max_xp <- dndR::xp_pool(party_level = party_level, party_size = party_size, difficulty = difficulty)
@@ -35,75 +39,109 @@ encounter_creator <- function(party_level = NULL, party_size = NULL, difficulty 
   # Put a ceiling on that to be able to include more creatures
   capped_xp <- ceiling(x = max_xp - (max_xp * 0.65))
 
-  # Pick the difficulty of the first (hardest) creature in the encounter
-  picked <- available %>%
-    # Choose highest possible XP that is still less than the capped XP
-    dplyr::filter(creature_xp == max(creature_xp[creature_xp < capped_xp]))
+  # Make an empty list
+  attempt_list <- list()
 
-  # Calculate spent XP for this creature
-  spent_xp <- xp_cost(monster_xp = sum(picked$creature_xp),
-                      monster_count = nrow(picked),
-                      party_size = party_size)
+  # Loop across specified number of attempts
+  for(k in 1:try){
 
-  # Update set of available creatures
-  available %<>%
-    # XP value less than (or equal to) remaining XP
-    dplyr::filter(creature_xp <= (max_xp - spent_xp) &
-                    # And not as tough as the first chosen creature
-                    creature_xp < max(picked$creature_xp))
+    # Re-set the random seed
+    set.seed(seed = sample(x = 10^3, size = 1))
 
-  # While there is XP to spend and creatures to spend it on, do so
-  while(spent_xp < max_xp & nrow(available) >= 1){
+    # Identify all possible XP values for creatures
+    available <- data.frame("creature_xp" = c(10, 25, 50, 100, 200, 450,
+                                              700, 1100, 1800, 2300, 2900,
+                                              3900, 5000, 5900, 7200, 8400,
+                                              8900, 10000, 11500, 13000,
+                                              15000, 18000, 20000, 22000, 25000,
+                                              33000, 41000, 50000, 62000, 62500,
+                                              75000, 90000, 105000, 155000, 330000))
 
-    # Identify XP levels of remaining available creatures
-    xp_levels <- unique(available$creature_xp)
+    # Pick the difficulty of the first (hardest) creature in the encounter
+    picked <- available %>%
+      # Choose highest possible XP that is still less than the capped XP
+      dplyr::filter(creature_xp == max(creature_xp[creature_xp < capped_xp]))
 
-    # Pick a random XP value
-    ## Need to do this as a conditional because `sample` is unreliable when length is < 1
-    if(length(xp_levels) > 1){
-      xp_value <- sample(x = xp_levels, size = 1)
-    } else { xp_value <- xp_levels }
+    # Calculate spent XP for this creature
+    spent_xp <- xp_cost(monster_xp = sum(picked$creature_xp),
+                        monster_count = nrow(picked),
+                        party_size = party_size)
 
-    # See if including a creature of that XP is still below the threshold
-    (possible_cost <- xp_cost(monster_xp = sum(c(picked$creature_xp, xp_value)),
-                              monster_count = nrow(picked) + 1,
-                              party_size = party_size))
+    # Update set of available creatures
+    available %<>%
+      # XP value less than (or equal to) remaining XP
+      dplyr::filter(creature_xp <= (max_xp - spent_xp) &
+                      # And not as tough as the first chosen creature
+                      creature_xp < max(picked$creature_xp))
 
-    # If this would be below the maximum allowed XP...
-    if(possible_cost < max_xp){
+    # While there is XP to spend and creatures to spend it on, do so
+    while(spent_xp < max_xp & nrow(available) >= 1){
 
-      # Choose one candidate creature
-      candidate <- available %>%
-        dplyr::filter(creature_xp == xp_value)
+      # Identify XP levels of remaining available creatures
+      xp_levels <- unique(available$creature_xp)
 
-      # Add this to the picked set of creatures
-      picked <- picked %>%
-        dplyr::bind_rows(candidate)
+      # Pick a random XP value
+      ## Need to do this as a conditional because `sample` is unreliable when length is < 1
+      if(length(xp_levels) > 1){
+        xp_value <- sample(x = xp_levels, size = 1)
+      } else { xp_value <- xp_levels }
 
-      # Update the 'spent XP' object
-      spent_xp <- possible_cost
+      # See if including a creature of that XP is still below the threshold
+      (possible_cost <- xp_cost(monster_xp = sum(c(picked$creature_xp, xp_value)),
+                                monster_count = nrow(picked) + 1,
+                                party_size = party_size))
 
-      # If this would be *over* the allowed XP...
-    } else {
+      # If this would be below the maximum allowed XP...
+      if(possible_cost < max_xp){
 
-      # Remove all creatures of this value from the set of available creatures
-      available %<>%
-        dplyr::filter(creature_xp != xp_value)
+        # Choose one candidate creature
+        candidate <- available %>%
+          dplyr::filter(creature_xp == xp_value)
 
-    } # Close `else`
-  } # Close while loop
+        # Add this to the picked set of creatures
+        picked <- picked %>%
+          dplyr::bind_rows(candidate)
 
-  # Final processing
-  encounter_info <- picked %>%
-    # Count number of creatures per XP value
-    dplyr::group_by(creature_xp) %>%
-    dplyr::summarize(creature_count = dplyr::n()) %>%
-    dplyr::ungroup() %>%
-    # Arrange by XP
-    dplyr::arrange(dplyr::desc(creature_xp)) %>%
-    # Add some nice diagnostics to the picked dataframe
-    dplyr::mutate(encounter_xp_pool = max_xp,
-                  encounter_xp_cost = spent_xp)
+        # Update the 'spent XP' object
+        spent_xp <- possible_cost
+
+        # If this would be *over* the allowed XP...
+      } else {
+
+        # Remove all creatures of this value from the set of available creatures
+        available %<>%
+          dplyr::filter(creature_xp != xp_value)
+
+      } # Close `else`
+    } # Close while loop
+
+    # Processing attempt information and storing in list
+    attempt_list[[paste0("attempt_", k)]] <- picked %>%
+      # Count number of creatures per XP value
+      dplyr::group_by(creature_xp) %>%
+      dplyr::summarize(creature_count = dplyr::n()) %>%
+      dplyr::ungroup() %>%
+      # Arrange by XP
+      dplyr::arrange(dplyr::desc(creature_xp)) %>%
+      # Add some nice diagnostics to the picked dataframe
+      dplyr::mutate(max_xp = max_xp,
+                    spent_xp = spent_xp,
+                    attempt_num = k)
+
+  } # Close attempt loop
+
+  # Wrangle attempt information, beginning with unlisting the attempt list
+  encounter_info <- purrr::list_rbind(x = attempt_list) %>%
+    # Keep only the attempt(s) closest to the maximum
+    dplyr::filter(spent_xp == max(spent_xp)) %>%
+    # Filter to only the first attempt that worked this way
+    dplyr::filter(attempt_num == min(attempt_num)) %>%
+    # Clean the resulting dataframe up a bit
+    ## Rename columns more informatively
+    dplyr::rename(encounter_xp_pool = max_xp,
+                  encounter_xp_cost = spent_xp) %>%
+    ## Drop attempt number column
+    dplyr::select(-attempt_num)
 
   # Return encounter information
   return(encounter_info) }
