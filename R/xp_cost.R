73fad1db54db15731b7790f8ef99747e0995408a
njlyon0 <njlyon@alumni.iastate.edu>
2025-04-02 10:36:52 -0400

    feat (encounters): function updates for 2024 rules (see #21)

--- R/xp_cost.R
+++ R/xp_cost.R
@@ -1,28 +1,30 @@
 #' @title Adjust the XP Total by Number of Monsters and Party Size
 #'
-#' @description Encounters are more difficult than the total of the monsters' experience points (XP). Both the number of monsters making attacks and the number of players attacking those creatures can affect the difficulty of an encounter. The Dungeon Master's Guide (DMG) accounts for this by providing an XP multiplier for given party sizes and numbers of monsters. This function accepts the unmodified total of the monsters' XP and adjusts this as specified in the DMG without the pain of the tables in that book.
+#' @description In the 2014 version of fifth edition Dungeons and Dragons, encounters are more difficult than the total of the monsters' experience points (XP). Both the number of monsters making attacks and the number of players attacking those creatures can affect the difficulty of an encounter. The 2014 Dungeon Master's Guide (DMG) accounts for this by providing an XP multiplier for given party sizes and numbers of monsters. This function accepts the unmodified total of the monsters' XP and adjusts this as specified in the DMG without the pain of the tables in that book. For the 2024 version, the XP cost is exactly equal to the total monster XP so this function will return a warning and whatever value is passed to the `monster_xp` argument if the 2024 version is specified.
 #'
 #' @param monster_xp (numeric) XP total across all monsters
 #' @param monster_count (numeric) count for the number of monsters in the encounter
 #' @param party_size (numeric) value for the number of PCs in the party
-#'
+#' @param ver (character) which version of fifth edition to use ("2014" or "2024")
+#' 
 #' @return (numeric) value for "realized" XP
+#' 
 #' @importFrom magrittr %>%
 #'
 #' @export
 #'
 #' @examples
 #' # Calculate the realized XP from the raw XP, number of monsters, and number of PCs
-#' dndR::xp_cost(monster_xp = 100, monster_count = 3, party_size = 2)
+#' dndR::xp_cost(monster_xp = 100, monster_count = 3, party_size = 2, ver = "2014")
 #'
-xp_cost <- function(monster_xp = NULL, monster_count = NULL, party_size = NULL){
+xp_cost <- function(monster_xp = NULL, monster_count = NULL, party_size = NULL, ver = "2014"){
   # Squelch visible bindings note
   monster_number <- party_category <- NULL
-
+  
   # Monster XP has to be a single numeric value
   if(is.null(monster_xp) || is.numeric(monster_xp) != TRUE || length(monster_xp) != 1)
     stop("'monster_xp' must be specified as a single number")
-
+  
   # Same error for number of monsters
   if(is.null(monster_count) || is.numeric(monster_count) != TRUE || length(monster_count) != 1)
     stop("'monster_count' must be specified as a single number")
@@ -30,6 +32,14 @@ xp_cost <- function(monster_xp = NULL, monster_count = NULL, party_size = NULL){
   # Same error check for party size
   if(is.null(party_size) || is.numeric(party_size) != TRUE || length(party_size) != 1)
     stop("'party_size' must be defined as a single number")
+  
+  # Similar error check for version
+  if(is.null(ver) || is.character(ver) != TRUE || length(ver) != 1 || ver %in% c("2014", "2024") != TRUE)
+    stop("'ver' must be one of either '2014' or '2024'")
+  
+  # Warning for non-2014 version
+  if(ver == "2024")
+    warning("The 2024 version of fifth edition D&D does not modify monster XP based on either number of monsters or party size")
   
   # Assemble multiplier table (see p. 82 in 2014 DMG)
   multiplier_table_raw <- base::data.frame(
@@ -47,32 +57,34 @@ xp_cost <- function(monster_xp = NULL, monster_count = NULL, party_size = NULL){
                         values_to = "multiplier") %>%
     dplyr::mutate(monster_number = base::as.numeric(
       base::gsub(pattern = "monster_", replacement = "", monster_number)))
-
+  
   # Subset based on party size
   if(party_size < 3){
     multiplier_table_sub <- dplyr::filter(multiplier_table_raw, party_category == "small") }
-
+  
   if(party_size >= 3 & party_size <= 5){
     multiplier_table_sub <- dplyr::filter(multiplier_table_raw, party_category == "typical") }
-
+  
   if(party_size > 5){
     multiplier_table_sub <- dplyr::filter(multiplier_table_raw, party_category == "large") }
-
+  
   # If the monster count is greater than 16, cap it there
   if(monster_count > 16){
     warning("DMG table only provides information on up to 16 monsters. Setting monster count to 16")
     monster_count <- 16
-    }
-
+  }
+  
   # Subset further based on number of monsters
   multiplier_actual <- dplyr::filter(multiplier_table_sub,
                                      monster_number == monster_count)$multiplier
-
+  
   # Multiply the provided XP by the identified multiplier
   xp_mult <- (monster_xp * multiplier_actual)
-
+  
   # Round it to the nearest integer
-  xp_actual <- base::trunc(x = xp_mult)
-
-  # Return the multiplied XP
-  return(xp_actual) }
+  xp_actual <- round(x = xp_mult, digits = 0)
+  
+  # Return the relevant value of XP for the specified version
+  if(ver == "2014"){
+    return(xp_actual)
+  } else { return(monster_xp) } }
--- R/xp_pool.R
+++ R/xp_pool.R
@@ -1,20 +1,26 @@
 #' @title Calculate Total XP of Monsters for Given Party Level and Difficulty
 #'
-#' @description Returns the total XP (experience points) of all creatures that would make an encounter the specified level of difficulty for a party of the supplied level. This 'pool' can be used by a GM (game master) to "purchase" monsters to identify how many a party is likely to be able to handle given their average level. NOTE: this does not take into account creature-specific abilities or traits so care should be taken if a monster has many such traits that modify its difficulty beyond its experience point value.
+#' @description Returns the total XP (experience points) of all creatures that would make an encounter the specified level of difficulty for a party of the supplied level. This 'pool' can be used by a GM (game master) to "purchase" monsters to identify how many a party is likely to be able to handle given their average level. This function supports both 2014 and 2024 versions of fifth edition D&D. Note that the returned pool does not take into account creature-specific abilities or traits so care should be taken if a monster has many such traits that modify its difficulty beyond its experience point value.
 #'
 #' @param party_level (numeric) integer indicating the _average_ party level. If all players are the same level, that level is the average party level. Non-integer values are supported but results will be slightly affected
 #' @param party_size (numeric) integer indicating how many player characters (PCs) are in the party
-#' @param difficulty (character) one of "easy", "medium", "hard", or "deadly" for the desired difficulty of the encounter.
+#' @param ver (character) which version of fifth edition to use ("2014" or "2024")
+#' @param difficulty (character) one of a specific set of encounter difficulty level names. If `ver = "2014"`, this must be one of "easy", "medium", "hard", or "deadly" while for `ver = "2024"` it must instead be one of "low", "moderate", or "high"
 #'
 #' @return (numeric) total encounter XP as an integer
 #' @export
 #'
 #' @examples
 #' # Supply a party level and difficulty and get the total XP of such an encounter
-#' dndR::xp_pool(party_level = 3, party_size = 2, difficulty = 'medium')
+#' dndR::xp_pool(party_level = 3, party_size = 2, ver = "2014", difficulty = "medium")
+#' 
+#' # Calculate the XP pool for the same party under the 2024 rules
+#' xp_pool(party_level = 3, party_size = 2, ver = "2024", difficulty = "moderate")
 #'
-xp_pool <- function(party_level = NULL, party_size = NULL, difficulty = NULL){
-
+xp_pool <- function(party_level = NULL, party_size = NULL, ver = NULL, difficulty = NULL){
+  # Squelch 'visible bindings' note
+  pc_level <- xp <- NULL
+  
   # Party level must be specified as a single number
   if(is.null(party_level) || is.numeric(party_level) != TRUE || length(party_level) != 1)
     stop("'party_level' must be defined as a single number")
@@ -23,55 +29,85 @@ xp_pool <- function(party_level = NULL, party_size = NULL, difficulty = NULL){
   if(is.null(party_size) || is.numeric(party_size) != TRUE || length(party_size) != 1)
     stop("'party_size' must be defined as a single number")
   
+  # Similar error check for version
+  if(is.null(ver) || is.character(ver) != TRUE || length(ver) != 1 || ver %in% c("2014", "2024") != TRUE)
+    stop("'ver' must be one of either '2014' or '2024'")
+  
   # Make sure difficulty is lowercase
   diff_lcase <- tolower(x = difficulty)
   
-  # Difficulty must be provided as a single value and be a one of the specified levels
-  if(is.null(diff_lcase) || length(diff_lcase) != 1 || diff_lcase %in% c("easy", "medium", "hard", "deadly") != TRUE)
-    stop("'difficulty' must be provided as one of 'easy', 'medium', 'hard', or 'deadly'")
+  # Regardless of version, difficulty must be provided as a single value
+  if(is.null(diff_lcase) || length(diff_lcase) != 1 || is.character(diff_lcase) != TRUE)
+    stop("'difficulty' must be provided as a single character value")
   
-  # If party level is an integer, use the DMG's values
-  if(stringr::str_detect(string = party_level, pattern = "\\.") == TRUE){
-
-    # Define XP / player values from DMG
-    xp_df <- data.frame('pc_level' = 1:20,
-                        'easy_xp' = c(25, 50, 75, 125, 250, 300, 350, 450,
-                                      550, 600, 800, 1000, 1100, 1250, 1400,
-                                      1600, 2000, 2100, 2400, 2800))
-
-    # Grab the appropriate one
-    easy_xp_amount <- xp_df[xp_df$pc_level == party_level, ]$easy_xp
-
-    # If party level is *not* an integer, calculate by hand
-  } else {
-
-    # Identify the quadratic coefficients
-    ## (Did this manually and got a decent fit for the line)
-    a <- 8.216374
-    b <- -26.49122
-    c <- 43.27485
-
-    # Calculate XP value for adventurers of this level (for an easy encounter)
-    base_xp_amount <- ( (a * (party_level^2)) + (b * party_level) + c )
-
-    # The formula for the line isn't perfect so we have another quick modification to make
-    if(party_level < 2) { easy_xp_amount <- base_xp_amount }
-    if(party_level >= 2 & party_level < 12) { easy_xp_amount <- (base_xp_amount + 55) }
-    if(party_level >= 12) { easy_xp_amount <- base_xp_amount }
-
-  } # Close player level integer vs. not conditional
-
-  # Now multiply as needed for the desired difficulty (this is straight from the DMG's table)
-  if(difficulty == "easy"){ single_xp_amount <- easy_xp_amount }
-  if(difficulty == "medium"){ single_xp_amount <- (easy_xp_amount * 2) }
-  if(difficulty == "hard"){ single_xp_amount <- (easy_xp_amount * 3) }
-  if(difficulty == "deadly"){ single_xp_amount <- (easy_xp_amount * 4) }
-
-  # Multiply that by the number of PCs in the party
-  xp_amount <- (single_xp_amount * party_size)
-
-  # And round that value to not return weird decimals
-  xp_actual <- base::trunc(x = xp_amount)
-
-  # Return the xp_amount
+  # 5e 2014 version ----
+  # Calculate XP pool for the 2014 version of 5e
+  if(ver == "2014") {
+    
+    # Error out if difficulty is not a supported level for this version of D&D
+    if(diff_lcase %in% c("easy", "medium", "hard", "deadly") != TRUE)
+      stop("For the 2014 version, 'difficulty' must be one of 'easy', 'medium', 'hard', 'deadly'")
+    
+    # Define XP-player level table from 2014 DMG
+    xp14_df <- data.frame("pc_level" = 1:20,
+                          "xp" = c(25, 50, 75, 125, 250, # 1-5
+                                   300, 350, 450, 550, 600, # 6-10
+                                   800, 1000, 1100, 1250, 1400, # 11-15
+                                   1600, 2000, 2100, 2400, 2800)) # 16-20
+    
+    # Grab the highest XP value without exceeding the player's average level
+    easy_xp <- xp14_df %>% 
+      dplyr::filter(pc_level <= party_level) %>% 
+      utils::tail(n = 1) %>% 
+      dplyr::pull(xp)
+    
+    # Now multiply as needed for the desired difficulty
+    ## Straight from a table in the DMG
+    if(diff_lcase == "easy"){ pc_xp <- easy_xp }
+    if(diff_lcase == "medium"){ pc_xp <- (easy_xp * 2) }
+    if(diff_lcase == "hard"){ pc_xp <- (easy_xp * 3) }
+    if(diff_lcase == "deadly"){ pc_xp <- (easy_xp * 4) }
+    
+  } # Close 2014 version conditional
+  
+  # 5e 2024 version ----
+  if(ver == "2024"){
+    
+    # Error out if difficulty is not a supported level for this version of D&D
+    if(diff_lcase %in% c("low", "moderate", "high") != TRUE)
+      stop("For the 2024 version, 'difficulty' must be one of 'low', 'moderate', 'high'")
+    
+    # Define XP-player level table from 2024 DMG
+    xp24_df <- data.frame("pc_level" = 1:20,
+                          "low" = c(50, 100, 150, 250, 500, # 1-5
+                                    600, 750, 1000, 1300, 1600, # 6-10
+                                    1900, 2200, 2600, 2900, 3300, # 11-15
+                                    3800, 4500, 5000, 5500, 6400), # 16-20
+                          "moderate" = c(75, 150, 225, 375, 750, # 1-5
+                                         1000, 1300, 1700, 2000, 2300, # 6-10
+                                         2900, 3700, 4200, 4900, 5400, # 11-15
+                                         6100, 7200, 8700, 10700, 13200), # 16-20
+                          "high" = c(100, 200, 400, 500, 1100, # 1-5
+                                     1400, 1700, 2100, 2600, 3100, # 6-10
+                                     4100, 4700, 5400, 6200, 7800, # 11-15
+                                     9800, 11700, 14200, 17200, 22000)) # 16-20
+    
+    # Process that table
+    xp_row <- xp24_df %>%
+      # Grab the single highest player level without exceeding the party's average level
+      dplyr::filter(pc_level <= party_level) %>% 
+      utils::tail(n = 1)
+    
+    # Grab the XP column corresponding to the desired difficulty
+    pc_xp <- xp_row[[diff_lcase]]
+    
+  } # Close 2024 version conditional
+  
+  # Regardless of version, multiply per-player XP by the number of PCs in the party
+  party_xp <- (pc_xp * party_size)
+  
+  # And round that to the nearest integer
+  xp_actual <- round(x = party_xp, digits = 0)
+  
+  # Return the calculated party-level XP
   return(xp_actual) }
