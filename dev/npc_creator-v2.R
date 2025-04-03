c9ae9b5a3d537799065282ecb2b5ecdade08d6ca
njlyon0 <njlyon@alumni.iastate.edu>
2025-04-03 11:23:44 -0400

    dev: script skeleton for improvements to `npc_creator` (see #28)

---
+++ dev/npc_creator-v2.R
@@ -0,0 +1,25 @@
+## ---------------------------------------------------- ##
+# NPC Creator Function Upgrade
+## ---------------------------------------------------- ##
+
+# Billy (William) Mitchell (https://github.com/wj-mitchell) forked dndR and has been working on some new/upgraded functions
+## The coolest / most-easily integratable (IMO) is his upgrades to the `npc_creator` function
+## Working here on integrating his version with mine to get that improvement ASAP
+
+## ------------------------------- ##
+# Housekeeping ----
+## ------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, dndR)
+
+# Clear environment
+rm(list = ls()); gc()
+
+## ------------------------------- ##
+# Upgrade Dev Area ----
+## ------------------------------- ##
+
+
+
+# End ----
